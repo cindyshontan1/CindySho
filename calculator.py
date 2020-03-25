01a08eac787b3a7de1991bdf2e482b81e7039fa1
cindyshontan1 <cindyshontan@gmail.com>
2020-03-25 18:25:20 +0100

    first python commit

---
+++ calculator.py
@@ -0,0 +1,6 @@
+print('''Hello Davis, I will be computing the Area of a circle, ''' )
+radius_choice = float(input('Kindly input the radius of a circle of your choice: ' ))
+pie = 3.14159
+area_choice = (pie * radius_choice * radius_choice)
+print("The area of the circle is:", area_choice)
+print('Thank you')
